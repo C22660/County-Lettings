0c1290e4834a77571442291f99d54fe6be9dd593
Cedric Morgantini <c.morgantini@free.fr>
2022-02-01 12:13:46 +0100

    Ajout des admin dans lettings et profiles et suppression de celui de oc_lettings_site

--- lettings/admin.py
+++ lettings/admin.py
@@ -1,13 +1,13 @@
-# from django.contrib import admin
-#
-# from lettings.models import Letting, Address
-#
-#
-# @admin.register(Letting)
-# class LettingAdmin(admin.ModelAdmin):
-#     pass
-#
-#
-# @admin.register(Address)
-# class AddressAdmin(admin.ModelAdmin):
-#     pass
+from django.contrib import admin
+
+from lettings.models import Letting, Address
+
+
+@admin.register(Letting)
+class LettingAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    pass
